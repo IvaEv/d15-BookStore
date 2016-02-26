343ae299c4787181b1089002190ef1178f187821
zaverden <zaverden@gmail.com>
2016-02-26 20:36:21 +0700

    first lesson

---
+++ BookStore.ViewModels/LoginViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System.ComponentModel.Composition;
+using System.Windows.Input;
+using IkitMita;
+using IkitMita.Mvvm.ViewModels;
+
+namespace BookStore.ViewModels
+{
+    [Export]
+    public class LoginViewModel : ChildViewModelBase
+    {
+        private ICommand _makeLoginCommand;
+        private string _login;
+        private string _password;
+        private string _message;
+
+        public LoginViewModel()
+        {
+            Title = "Авторизация";
+        }
+
+        public string Login
+        {
+            get { return _login; }
+            set
+            {
+                _login = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Message
+        {
+            get { return _message; }
+            set
+            {
+                _message = value;
+                OnPropertyChanged();
+            }
+        }
+
+        [DependsOn(nameof(Login))]
+        [DependsOn(nameof(Password))]
+        public ICommand MakeLoginCommand
+        {
+            get
+            {
+                return _makeLoginCommand ??
+                       (_makeLoginCommand = new DelegateCommand(MakeLogin, 
+                       () => !Login.IsNullOrEmpty() && !Password.IsNullOrEmpty()));
+            }
+        }
+
+        private void MakeLogin()
+        {
+            if (Login == Password)
+            {
+                Message = "Авторизация успешна";
+            }
+            else
+            {
+                Message = "Логин или пароль введены неправильно";
+            }
+        }
+    }
+}
