29b7e8cccc24ceda878bd04b2b3caf1af8ca6c31
Denis Zavershinskiy <zaverden@gmail.com>
2016-04-23 11:19:09 +0700

    dumb employees controller

---
+++ BookStore.WebService/Controllers/EmployeesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using BookStore.DataAccess.EF.Operations;
+using BookStore.DataAccess.Models;
+
+namespace BookStore.WebService.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        public async Task<GetEmployeeModel> GetAsync(int id)
+        {
+            var operation = new GetEmployeeOperation();
+            var employeeModel = await operation.ExecuteAsync(id);
+            return employeeModel;
+        }
+    }
+}
